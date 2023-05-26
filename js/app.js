964f8712e33ab1c9e33cfc9166df9b3058a0eb6a
ReneGlezCota <reneglezcota@gmail.com>
2023-05-26 10:58:15 -0700

    v1 RickandMorty

--- js/app.js
+++ js/app.js
@@ -1,114 +1,48 @@
-const posts = [
-  {
-    userId: 1,
-    id: 1,
-    title:
-      "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-    body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-  },
-  {
-    userId: 1,
-    id: 2,
-    title: "qui est esse",
-    body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
-  },
-  {
-    userId: 1,
-    id: 3,
-    title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-    body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
-  },
-];
+document.addEventListener("DOMContentLoaded", () => {
+  fetchData();
+});
 
-// const findPostById = (id, callback) => {
-//   const post = posts.find((item) => item.id === id);
-//   if (post) {
-//     callback(null, post);
-//   } else {
-//     callback("No se encontro el post con id " + id);
-//   }
-// };
-
-// findPostById(1, (err, post) => {
-//   if (err) return console.log(err);
-//   console.log(post);
-
-//   findPostById(2, (err, post) => {
-//     if (err) return console.log(err);
-//     console.log(post);
-
-//     findPostById(3, (err, post) => {
-//       if (err) return console.log(err);
-//       console.log(post);
-
-//       findPostById(4, (err, post) => {
-//         if (err) return console.log(err);
-//         console.log(post);
-//       });
-//     });
-//   });
-// });
-
-// const findPostById = (id) => {
-//   const post = posts.find((item) => item.id === id);
-//   return new Promise((resolve, reject) => {
-//     if (post) {
-//       resolve(post);
-//     } else {
-//       reject("No se encontro el post con id " + id);
-//     }
-//   });
-// };
-
-const findPostById = (id) =>
-  new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const post = posts.find((item) => item.id === id);
-      if (post) {
-        resolve(post);
-      } else {
-        reject("No se encontro el post con id " + id);
-      }
-    }, 2000);
-  });
-
-// findPostById(1)
-//   .then((post) => {
-//     console.log(post);
-//   })
-//   .catch((e) => console.log(e));
-
-const buscar = async (id) => {
+const fetchData = async () => {
   try {
-    const resPosts = await Promise.all([findPostById(1), findPostById(2)]);
-    console.log(resPosts);
-    // const post = await findPostById(id);
-    // console.log(post);
+    loadingData(true);
+
+    const url = "https://rickandmortyapi.com/api/character";
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    pintarCard(data);
   } catch (error) {
     console.log(error);
   } finally {
-    console.log("Se ejecuta si o si");
+    loadingData(false);
   }
 };
 
-//buscar(5);
-const url = "https://jsonplaceholder.typicode.com/posts/";
+const pintarCard = (data) => {
+  const cards = document.getElementById("card-dinamicas");
+  const templateCard = document.getElementById("template-card");
+  const fragment = document.createDocumentFragment();
+  // console.log(data)
+  data.results.forEach((item) => {
+    // console.log(item);
+    const clone = templateCard.content.cloneNode(true);
+    clone.querySelector("h5").textContent = item.name;
+    clone.querySelector("p").textContent = item.species;
+    clone.querySelector(".card-img-top").setAttribute("src", item.image);
 
-// fetch(url)
-//   .then((res) => res.json())
-//   .then((data) => console.log(data))
-//   .catch((e) => console.log(e))
-//   .finally(() => console.log("Finalizo"));
-
-const findPostById2 = async (id) => {
-  try {
-    const res = await fetch(url + id);
-    const data = await res.json();
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
+    //Guardamos en el fragment para evitar el reflow
+    fragment.appendChild(clone);
+  });
+  cards.appendChild(fragment);
 };
 
-findPostById2(2);
-console.log("Fin del codigo");
+const loadingData = (estado) => {
+  const loading = document.getElementById("loading");
+
+  if (estado) {
+    loading.classList.remove("d-none");
+  } else {
+    loading.classList.add("d-none");
+  }
+};
